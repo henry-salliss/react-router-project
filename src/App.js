829d77036e9d44cccc810ec4fd83bd1ceb94f438
Henry Salliss <henrysalliss@gmail.com>
2022-01-12 18:46:50 +0000

    added navigation and layout components, added nested route to quote details page, added redirect in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import QuoteDetails from "./pages/QuoteDetails";
 import Quotes from "./pages/Quotes";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path={"/quotes"} exact>
-        <Quotes />
-      </Route>
-      <Route path={"/newquote"}>
-        <NewQuote />
-      </Route>
-      <Route path={"/quotes/:quoteID"}>
-        <QuoteDetails />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path={"/"} exact>
+          <Redirect to={"/quotes"} />
+        </Route>
+        <Route path={"/quotes"} exact>
+          <Quotes />
+        </Route>
+        <Route path={"/new-quote"}>
+          <NewQuote />
+        </Route>
+        <Route path={"/quotes/:quoteID"}>
+          <QuoteDetails />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import { Fragment } from "react";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetails = () => {
   const params = useParams();
   console.log(params);
 
-  return <p>{params.quoteID}</p>;
+  return (
+    <Fragment>
+      <p>{params.quoteID}</p>
+      <Route path={`/quotes/${params.quoteID}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetails;
