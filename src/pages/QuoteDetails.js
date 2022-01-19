86e68953a01de8eca79540f28e8bdd7d0dc95291
Henry Salliss <henrysalliss@gmail.com>
2022-01-19 19:20:56 +0000

    added more leaner routing code using useRouteMatch

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,10 +1,11 @@
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const QuoteDetails = () => {
   const params = useParams();
+  const match = useRouteMatch();
 
   const dummyData = [
     { id: "q1", author: "Henry Salliss", text: "bruh" },
@@ -16,14 +17,14 @@ const QuoteDetails = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteID}`} exact>
+      <Route path={`${match.url}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`/quotes/${params.quoteID}/comments`}>
             Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteID}/comments`}>
+      <Route path={`${match.url}/comments`}>
         <Comments />
       </Route>
     </Fragment>
